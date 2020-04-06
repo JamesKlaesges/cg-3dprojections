6e8bde7f41b3cc602da658ecaa96b47786facc25
JamesKlaesges <50339142+JamesKlaesges@users.noreply.github.com>
2020-04-06 13:59:56 -0700

    multiplying vertices

--- scripts/renderscene.js
+++ scripts/renderscene.js
@@ -92,6 +92,8 @@ function Animate(timestamp) {
         for (let j = 0; j < scene.models[i].vertices.length; j++)
         {
             var vertex = Matrix.multiply([scene.models[i].matrix, scene.models[i].vertices[j]]);
+            console.log(scene.models[i].matrix);
+            console.log(scene.models[i].vertices[j]);
             console.log(vertex);
             scene.models[i].vertices[j] = Vector4(vertex.x, vertex.y, vertex.z, vertex.w);
             console.log(scene.models[i].vertices[j]);
