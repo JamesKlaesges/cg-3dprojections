f8ce40ad34488ecf9de212b1ec55fc22520e7501
JamesKlaesges <50339142+JamesKlaesges@users.noreply.github.com>
2020-04-07 08:38:48 -0700

    Update transforms.js

--- scripts/transforms.js
+++ scripts/transforms.js
@@ -25,7 +25,6 @@ function Mat4x4Parallel(mat4x4, prp, srp, vup, clip) {
     var shear = new Matrix(4,4);
     var CW = Vector3((clip[0] + clip[1])/2, (clip[2] + clip[3])/2, -(clip[4]));
     var DOP = CW.subtract(prp);
-    DOP.normalize();
     var shx = -DOP.x/DOP.z;
     var shy = -DOP.y/DOP.z;
     Mat4x4ShearXY(shear, shx, shy);
@@ -70,7 +69,6 @@ function Mat4x4Projection(mat4x4, prp, srp, vup, clip) {
     var shear = new Matrix(4,4);
     var CW = Vector3((clip[0] + clip[1])/2, (clip[2] + clip[3])/2, -(clip[4]));
     var DOP = CW.subtract(prp);
-    DOP.normalize();
     var shx = -DOP.x/DOP.z;
     var shy = -DOP.y/DOP.z;
     Mat4x4ShearXY(shear, shx, shy);
